91ecc1a2657dea9edcf77fb8e4c22e1469d9571c
oleg-kovalchuk <dirt1457@gmail.com>
2017-02-15 16:46:46 +0200

    Update city.service.js

--- app/core/city/city.service.js
+++ app/core/city/city.service.js
@@ -6,15 +6,6 @@ angular.module('core.city').factory('City', function(Restangular) {
 			return Restangular.all('cities').getList().then(function(cities) {
 				return cities;
 			});
-		},
-		getNumber: function() {
-			var id = $stateParams.id || 0;
-
-			return Restangular.one('number', id).get().then(function(number) {
-				return number;
-			}, function (err) {
-				return err;
-			});
-		},
+		}
 	};
 });
